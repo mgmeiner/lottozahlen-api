c8e0624145ed1f5548c000532d848b57859ebf6b
michael <Michael.Gmeiner@witt-gruppe.eu>
2018-06-11 13:53:43 -0400

    introduce kotlin spring boot project

---
+++ src/main/kotlin/eu/mgmeiner/lottozahlenapi/LottozahlenApiApplication.kt
@@ -0,0 +1,11 @@
+package eu.mgmeiner.lottozahlenapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LottozahlenApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<LottozahlenApiApplication>(*args)
+}
